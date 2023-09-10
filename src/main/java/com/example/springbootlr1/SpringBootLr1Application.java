f8f0df9fc3b8c392861228c245df4bce29fe7c66
mihail <mihail_mihailow@list.ru>
2023-09-10 22:33:44 +0500

    Initial commit

---
+++ src/main/java/com/example/springbootlr1/SpringBootLr1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootlr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLr1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLr1Application.class, args);
+    }
+
+}
